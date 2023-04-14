c27262e06ff6b01ba8caee730fe526a12680650b
moisamidi <116157321+moisamidi@users.noreply.github.com>
2023-04-14 22:10:21 +0300

    change

--- yaremko_anna/product.py
+++ yaremko_anna/product.py
@@ -1,6 +1,6 @@
 class Product:
     def __init__(self, name, price):
-        "initialization of variables"
+        'initialization of variables'
         self._price = price
         self.name = name
 
@@ -10,7 +10,7 @@ class Product:
 
     @price.setter
     def price(self, price):
-        "checks price"
+        'checks price'
         if price < 0:
             raise ValueError("Price have to be >0")
         self._price = price
@@ -21,88 +21,101 @@ class Product:
 
     @name.setter
     def name(self, name):
-        "checks name of product"
+        'checks name of product'
         if not name.isalpha():
             raise ValueError("Name should contain only letters")
         self._name = name
 
 
 class Clothing(Product):
-    def __init__(self, name, price, size, gender):
-        "initialization of variables"
+    def __init__(self,name, price,  size, gender):
+        'initialization of variables'
         super().__init__(name, price)
         self.size = size
         self.gender = gender
-
+    
     def __str__(self):
-        "info about clothes"
-        return f"This {self.name} is for {self.gender} with size{self.size}"
-
+        'info about clothes'
+        return f'This {self.name} is for {self.gender} with size{self.size}'
+    
     @property
     def size(self):
         return self._size
 
     @size.setter
     def size(self, size):
-        example_size = ["XS", "S", "M", "L", "XL"]
+        example_size=['XS','S','M','L','XL']
         if size not in example_size:
             raise ValueError("Size can only be XS,S,M,L,XL ")
         self._size = size
 
-
-class Food(Product):
-    def __init__(self, name, price):
-        "initialization of variables"
+class Nutrition(Product):
+    type_of=None
+    def __init__(self,name, price):
+        'initialization of variables'
         super().__init__(name, price)
-        self.type = "Food"
 
 
-class Drink(Product):
-    def __init__(self, name, price):
-        "initialization of variables"
+class Food(Nutrition):
+    def __init__(self,name, price):
+        'initialization of variables'
         super().__init__(name, price)
-        self.type = "Drink"
+    type_of = "Food"
+    
+    def __str__(self):
+        return f'This {self.name} is {self.type_of} and costs {self.price}'
 
+
+class Drink(Nutrition):
+    def __init__(self, name, price):
+        'initialization of variables'
+        super().__init__(name, price)
+    type_of = "Drink"
+
+    def __str__(self):
+        return f'This {self.name} is {self.type_of} and costs {self.price}'
 
 class Book(Product):
     def __init__(self, name, price, genre, page_count):
-        "initialization of variables"
+        'initialization of variables'
         super().__init__(name, price)
         self.genre = genre
         self.page_count = page_count
 
     def __str__(self):
-        "info about book"
+        'info about book'
         return f"{self.name} ({self.genre}) - {self.page_count} pages"
-
 
 class Basket:
     def __init__(self):
-        "initialization of variables"
+        'initialization of variables'
         self.products = {}
 
     def add_product(self, name, quantity=1):
-        "adds products to a basket"
+        'adds products to a basket'
         if name in self.products:
             self.products[name] += quantity
         else:
             self.products[name] = quantity
 
     def remove_product(self, name, quantity=1):
-        "removes product from a basket"
+        'removes product from a basket'
         if name in self.products:
             if self.products[name] <= quantity:
                 del self.products[name]
             else:
                 self.products[name] -= quantity
         else:
-            return f"In basket there is no {name.lower()} "
-    
+            return f'In basket there is no {name.lower()} '
+
     @staticmethod
-    def get_total_price(self):
-        "calculates price"
+    def get_total_price(products):
+        'calculates price'
         suma = 0
         for name, quantity in products.items():
             suma += name.price * quantity
         return suma
+
+
+
 
